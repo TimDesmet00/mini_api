381a39a74d99aaf011c047711f86161487b87a51
tim <timdesmet00@gmail.com>
2023-12-06 11:39:55 +0100

    correction model MVC

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
 
-require_once 'vendor/autoload.php';
+// require_once 'vendor/autoload.php';
+try{
+    if (!empty($_GET['demande'])) {
+        $url = explode('/', filter_var($_GET['demande'], FILTER_SANITIZE_URL));
+        switch($url[0]){
+            case 'users': echo 'users';
+                break;
+            case 'roles': echo 'roles';
+                break;
+            default:
+                throw new Exception('La demande n\'est pas valide.');
+        }
+    } else {
+        throw new Exception('Problème de récupération de données.');
+    }
+} catch (Exception $e) {
+    $erreur =[
+        "message" => $e->getMessage(),
+        "code" => $e->getCode()
 
+    ];
+    print_r($erreur);
+}
